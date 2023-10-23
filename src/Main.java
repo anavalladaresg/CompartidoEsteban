0a1199711e0e8b0490ec672e4bce9ff67c94def9
DAM1 <emontesadraz@danielcastelao.org>
2023-10-23 08:45:49 +0200

    B

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,7 @@ public class Main {
 
         // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+8.
+            
             System.out.println("i = " + i);
         }
     }
