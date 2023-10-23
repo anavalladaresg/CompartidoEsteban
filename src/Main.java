e9a9d527264d41ab731b01d095f578814207caf8
Ana <avalladaresgonzalez@danielcastelao.org>
2023-10-23 08:51:40 +0200

    C

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,7 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
-            
             System.out.println("i = " + i);
         }
     }
